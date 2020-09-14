04e4fa4b4f106979fd8d03e1604ba8d42642826f
luis torres <tl224627@gmail.com>
2020-09-13 22:42:36 -0600

    Add project files.

---
+++ AccumulateMessagesFunction/AccumulateMessagesFunction.vb
@@ -0,0 +1,19 @@
+﻿'Luis Torres
+'RCET0265
+'spring 2020
+'Accumulate Messages Function
+'
+Option Strict On
+Option Explicit On
+Option Compare Text
+
+Module AccumulateMessagesFunction
+
+    Sub Main()
+        Dim userInput As String
+        Dim message As String
+        Dim clearData As Boolean
+
+    End Sub
+
+End Module
