a755bba708f174dbbc23c9e4c12b9fa961463a5e
luis torres <tl224627@gmail.com>
2020-09-13 23:11:46 -0600

    add some directions

--- AccumulateMessagesFunction/AccumulateMessagesFunction.vb
+++ AccumulateMessagesFunction/AccumulateMessagesFunction.vb
@@ -15,6 +15,34 @@ Module AccumulateMessagesFunction
         Dim message As String
         Dim clearData As Boolean
 
+        Console.WriteLine($"enter messages and they enter summon to store  or delet to delet messege")
+
+        Do
+            userInput = Console.ReadLine()
+            If userInput = "summon" Then
+                MsgBox(message)
+            ElseIf userInput = "delet" Then
+                clearData = True
+            End If
+
+            message = AccumulateMessage(userInput, clearData)
+
+            clearData = False
+
+        Loop
     End Sub
+
+    Function AccumulateMessage(ByVal newMessage As String, ByVal clear As Boolean) As String
+        Static userMessage As String
+        If clear = True Then
+            userMessage = ""
+        ElseIf newMessage = "summon" Then
+        Else
+            userMessage &= newMessage & vbNewLine
+        End If
+
+        Return userMessage
+
+    End Function
 
 End Module
